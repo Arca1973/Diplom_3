9181ae1eae32c88e65736ec24e534b2f7baaeafe
Arca1973 <6845829@mail.ru>
2025-04-03 17:28:42 +0300

    Correctionns BurgerConstructorPage

--- src/main/java/pages/BurgerConstructorPage.java
+++ src/main/java/pages/BurgerConstructorPage.java
@@ -13,9 +13,9 @@ public class BurgerConstructorPage {
     // Локатор
     private final By singInButton = By.xpath("//div[@class='button_button_33qZ0 button_button_type_primary_107bx button_button_size_large_G21vg']//button[text()='Войти в аккаут']"); //кнопка «Войти в аккаунт» на главной,
     private final By accountLink = By.xpath("//a[contains(@class, 'AppHeader_header__linkText__3q_va ml-2') and contains(text(), 'Личный Кабинет')]"); //ссылка «Личный кабинет» на главной,
-    private final By bunsLink = By.xpath("//span[contains(text(), 'Булки')]")//ссылка Булки
-    private final By saucesLink = By.xpath("//span[contains(text(), 'Соусы')]")//ссылка Соусы
-    private final By toppingsLink = By.xpath("//span[contains(text(), 'Начинки')]")//ссылка Начинки
+    private final By bunsLink = By.xpath("//span[contains(text(), 'Булки')]");//ссылка Булки
+    private final By saucesLink = By.xpath("//span[contains(text(), 'Соусы')]");//ссылка Соусы
+    private final By toppingsLink = By.xpath("//span[contains(text(), 'Начинки')]");//ссылка Начинки
 
 
     public void clickSingInButton() {
@@ -30,10 +30,11 @@ public class BurgerConstructorPage {
         driver.findElement(bunsLink).click();
     }
 
-    public void clickSaucesLink () {
-        driver.findElement(saucesLink ).click();
+    public void clickSaucesLink() {
+        driver.findElement(saucesLink).click();
     }
 
     public void clickToppingsLink() {
         driver.findElement(toppingsLink).click();
     }
+}
