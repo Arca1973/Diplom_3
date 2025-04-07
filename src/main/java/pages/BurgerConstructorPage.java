7f8909682bf2baf3d7093bf5a422d784cccd57e3
Arca1973 <6845829@mail.ru>
2025-04-07 15:06:43 +0300

    добавлены методы и тесты переходов по пунктам меню «Булки», «Соусы», «Начинки». MenuNavigationTests

--- src/main/java/pages/BurgerConstructorPage.java
+++ src/main/java/pages/BurgerConstructorPage.java
@@ -2,9 +2,7 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
+import org.openqa.selenium.WebElement;
 
 public class BurgerConstructorPage {
 
@@ -12,12 +10,16 @@ public class BurgerConstructorPage {
         this.driver = driver;
     }
     private final WebDriver driver;
-    // Локатор
+    // Локаторы
     private final By singInButton = By.xpath("//button[text()='Войти в аккаунт']"); //кнопка «Войти в аккаунт» на главной,
     private final By accountLink = By.xpath("//*[contains(text(), 'Личный Кабинет')]"); //ссылка «Личный кабинет» на главной,
     private final By bunsLink = By.xpath("//span[contains(text(), 'Булки')]");//ссылка Булки
     private final By saucesLink = By.xpath("//span[contains(text(), 'Соусы')]");//ссылка Соусы
     private final By toppingsLink = By.xpath("//span[contains(text(), 'Начинки')]");//ссылка Начинки
+
+    private final By h2BunsLink = By.xpath(" //h2[contains(text(),'Булки')]");//заголовок Булки
+    private final By h2SaucessLink = By.xpath("//h2[contains(text(), 'Соусы')]");//заголовок Соусы
+    private final By h2ToppingsLink = By.xpath("//h2[contains(text(), 'Начинки')]");//заголовок Начинки
 
 
     public void clickSingInButton() {
@@ -29,14 +31,41 @@ public class BurgerConstructorPage {
     }
 
     public void clickBunsLink() {
-        driver.findElement(bunsLink).click();
+        WebElement element = driver.findElement(bunsLink);
+        String noselectAttribute = element.getAttribute("noselect");
+        if (noselectAttribute != null) {
+            element.click();
+        }
     }
 
     public void clickSaucesLink() {
-        driver.findElement(saucesLink).click();
+        WebElement element = driver.findElement(saucesLink);
+        String noselectAttribute = element.getAttribute("noselect");
+        if (noselectAttribute != null) {
+            element.click();
+        }
     }
 
     public void clickToppingsLink() {
-        driver.findElement(toppingsLink).click();
+        WebElement element = driver.findElement(toppingsLink);
+        String noselectAttribute = element.getAttribute("noselect");
+        if (noselectAttribute != null) {
+            element.click();
+        }
+    }
+
+    public boolean isH2BunsDisplayed() {
+        WebElement element = driver.findElement(h2BunsLink);
+               return element.isDisplayed();
+    }
+
+    public boolean isH2SaucessDisplayed() {
+        WebElement element = driver.findElement(h2SaucessLink);
+        return element.isDisplayed();
+    }
+
+    public boolean isH2ToppingsLinkDisplayed() {
+        WebElement element = driver.findElement(h2ToppingsLink);
+        return element.isDisplayed();
     }
 }
