5508fbd12bf4cffdf45b39fd79d96d879b90347c
Arca1973 <6845829@mail.ru>
2025-04-10 09:30:20 +0300

    Добавлены классы моделей

---
+++ src/main/java/API/ServerResponseModel.java
@@ -0,0 +1,19 @@
+package org.example.api;
+
+public class ServerResponseModel {
+    public boolean success;
+    public String message;
+    public String accessToken;
+    public String refreshToken;
+
+    // Вложенный класс теперь статический
+
+    public static class User {
+        public String email;
+        public String name;
+    }
+
+    // Добавляем объект типа User
+    public User user;
+}
+
